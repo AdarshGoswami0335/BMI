4ee7b49f04f669c980123f733331d377fb8b3372
AdarshGoswami0335 <64965127+AdarshGoswami0335@users.noreply.github.com>
2020-06-19 15:45:59 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The result of the calculation is" + result);
+});
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+});
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
+app.post("/bmicalculator", function(req,res){
+    var a = Number(req.body.weight);
+    var b = Number(req.body.height);
+    var n = a/b;
+    res.send("Your BMI is" +n);
+});
